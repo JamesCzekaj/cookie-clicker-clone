671125d5b017c59cc13e163918ccd65bbe62a599
James <blazertron447@gmail.com>
2017-06-25 00:13:20 +0100

    First Commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,27 @@
+var cookie = document.querySelector("#cookie");
+var scoreDisplay = document.querySelector("#scoreDisplay");
+var autoClickButton = document.querySelector("#autoClickButton");
+
+var score = 0;
+
+cookie.addEventListener("click", function() {
+  score++;
+  scoreDisplay.textContent = score;
+})
+
+var hasAutoClick = false;
+
+autoClickButton.addEventListener("click", function() {
+
+  if (score > 50 && hasAutoClick === false) {
+    setInterval(function() {
+      score++;
+      scoreDisplay.textContent = score;
+    }, 1000);
+    this.classList.add("disabled", "disabled");
+    this.textContent = "Auto Click - Purchased";
+    hasAutoClick = true;
+  } else if (score < 50 && hasAutoClick === false) {
+    alert("You don't have enough cookies!");
+  }
+})
